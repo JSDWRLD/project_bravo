3c80267f5ad8c6885882222e0e1a6efbf1e47ead
jsdwrld <123333887+JSDWRLD@users.noreply.github.com>
2024-10-13 10:04:14 -0700

    Added a eslint disable

--- src/components/ProductListPage.jsx
+++ src/components/ProductListPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+// eslint-disable-next-line react/prop-types
 const ProductListPage = ({ category }) => {
     const navigate = useNavigate();
     const [searchTerm, setSearchTerm] = useState('');
