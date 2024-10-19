29f6c3df440674610ab584061b684e3c11388757
jsdwrld <123333887+JSDWRLD@users.noreply.github.com>
2024-10-19 14:12:56 -0700

    Created user and product schema

---
+++ server/data/Users.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcryptjs')
+
+const userAccounts = [
+    {
+        name: "testAdmin",
+        email: "admin@node.com",
+        password: bcrypt.hashSync("123456", 10),
+        isAdmin: true,
+    },
+    {
+        name: "testUser",
+        email: "user@node.com",
+        password: bcrypt.hashSync("123456", 10),
+    },
+]
+
+module.exports = users
