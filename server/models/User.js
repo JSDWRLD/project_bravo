4513908aca1162c784227c135cd087b03a235de3
jsdwrld <123333887+JSDWRLD@users.noreply.github.com>
2024-10-24 22:23:02 -0700

    Added user register, login, and middleware for getting user profile information

--- server/models/User.js
+++ server/models/User.js
@@ -26,5 +26,14 @@ userSchema.methods.matchPassword = async function(enteredPassword) {
     // Have to match hash
     return await bcrypt.compare(enteredPassword, this.password)
 }
+// Register Password Hashing and Storing
+userSchema.pre("save", async function(next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+})
 
 module.exports = mongoose.model('User', userSchema)
