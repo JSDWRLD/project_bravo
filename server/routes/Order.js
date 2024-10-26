75457773452e0c3ce09e0cbfae91838e64b73570
jsdwrld <123333887+JSDWRLD@users.noreply.github.com>
2024-10-26 10:39:12 -0700

    Added Order Routes

--- server/routes/Order.js
+++ server/routes/Order.js
@@ -2,10 +2,11 @@ const express = require('express');
 const AsyncHandler = require('express-async-handler');
 const protect = require('../middleware/Auth');
 const Order = require('../models/Order');
+const { findById } = require('../models/User');
 const orderRoute = express.Router();
 
 orderRoute.post(
-    "/",
+    '/',
     protect,
     AsyncHandler(async (req, res) => {
         const {
@@ -35,6 +36,62 @@ orderRoute.post(
 
             const createdOrder = await order.save();
             res.status(201).json(createdOrder);
+        }
+    })
+);
+
+// Update Order Payment Status
+orderRoute.put(
+    '/:id/payment',
+    protect,
+    AsyncHandler(async(req, res) => {
+        const orderToUpdate = await Order.findById(req.params.id);
+
+        if (orderToUpdate) {
+            orderToUpdate.isPaid = true;
+            orderToUpdate.paidAt = Date.now();
+            orderToUpdate.paymentResult = {
+                order_id: req.body.order_id,
+                status: req.body.status,
+                updated_time: req.body.updated_time,
+                email_address: req.body.email_address,
+            };
+
+            const updatedOrder = await orderToUpdate.save();
+            res.status(200).json(updatedOrder);
+        } else {
+            res.status(404);
+            throw new Error("ORDER NOT FOUND")
+        }
+    })
+);
+
+// Get By Specific User, for detailed page
+orderRoute.get(
+    '/',
+    protect,
+    AsyncHandler(async(req, res) => {
+        const orders = await Order.find({user:req.user._id}).sort({_id:-1});
+        if (orders) {
+            res.status(200).json(orders);
+        } else {
+            res.status(404);
+            throw new Error("ORDERS NOT FOUND");
+        }
+    })
+);
+
+// Get one order by ID
+orderRoute.get(
+    '/:id',
+    protect,
+    AsyncHandler(async(req, res) => {
+        const order = await Order.findById(req.params.id).populate("user", "email");
+        if (order) {
+            res.status(200).json(order);
+        } else {
+            res.status(404);
+            throw new Error("NO ORDER FOUND");
         }
     })
 );
