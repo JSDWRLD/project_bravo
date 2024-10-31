1d780b208ecd56b09422dd2cce6aed4e408bddad
jsdwrld <123333887+JSDWRLD@users.noreply.github.com>
2024-10-31 14:20:54 -0700

    Updated

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,4 +2,3 @@ export { default as Card } from './Card.jsx';
 export { default as Navbar } from './Navbar.jsx';
 export { default as ProductDetailed } from './ProductDetailed.jsx';
 export { default as ProductListPage } from './ProductListPage.jsx';
-export { default as UserDropdown } from './UserDropdown.jsx'; 
