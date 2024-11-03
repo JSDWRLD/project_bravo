6b05895fa03b622a509a6c74c9b091d42d38e63b
jsdwrld <123333887+JSDWRLD@users.noreply.github.com>
2024-11-02 22:17:27 -0700

    Fixed footer placement

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,26 +8,28 @@ export default function App() {
   const { userInfo } = userLoginReducer;
 
   return (
-    <main className="bg-black text-gray-400 min-h-screen">
+    <div className="flex flex-col min-h-screen bg-black text-gray-400">
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop/board-games" element={<ProductListPage category="board-games" />} />
-          <Route path="/shop/puzzles" element={<ProductListPage category="puzzles" />} />
-          <Route path="/shop/retro-games" element={<ProductListPage category="retro-games" />} />
-          <Route path="/shop/consoles" element={<ProductListPage category="consoles" />} />
-          <Route path="/shop/:category/product/:id" element={<ProductDetailed />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={userInfo ? <Navigate to="/" /> : <Login />} />
-          <Route path="/register" element={userInfo ? <Navigate to="/" /> : <Register />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/order-history" element={<OrderHistory />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop/board-games" element={<ProductListPage category="board-games" />} />
+            <Route path="/shop/puzzles" element={<ProductListPage category="puzzles" />} />
+            <Route path="/shop/retro-games" element={<ProductListPage category="retro-games" />} />
+            <Route path="/shop/consoles" element={<ProductListPage category="consoles" />} />
+            <Route path="/shop/:category/product/:id" element={<ProductDetailed />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={userInfo ? <Navigate to="/" /> : <Login />} />
+            <Route path="/register" element={userInfo ? <Navigate to="/" /> : <Register />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/order-history" element={<OrderHistory />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </main>
         <Footer />
       </Router>
-    </main>
+    </div>
   );
 }
